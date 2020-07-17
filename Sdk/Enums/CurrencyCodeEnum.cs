8575ec43ec054556ed387a10010a638409c94016
Marek Polák <marazt@gmail.com>
2020-07-17 19:10:32 +0200

    Added autogenerated comments.

--- Sdk/Enums/CurrencyCodeEnum.cs
+++ Sdk/Enums/CurrencyCodeEnum.cs
@@ -10,201 +10,895 @@ namespace GPWebpayNet.Sdk.Enums
         /// The eur
         /// </summary>
         EUR = 978,
+
         /// <summary>
         /// The CZK
         /// </summary>
         CZK = 203,
+
         /// <summary>
         /// The GBP
         /// </summary>
         GBP = 826,
+
         /// <summary>
         /// The huf
         /// </summary>
         HUF = 348,
+
         /// <summary>
         /// The PLN
         /// </summary>
         PLN = 985,
+
         /// <summary>
         /// The rub
         /// </summary>
         RUB = 643,
+
         /// <summary>
         /// The usd
         /// </summary>
         USD = 840,
+
+        /// <summary>
+        /// The aed
+        /// </summary>
         AED = 784,
+
+        /// <summary>
+        /// The afn
+        /// </summary>
         AFN = 971,
+
+        /// <summary>
+        /// All
+        /// </summary>
         ALL = 008,
+
+        /// <summary>
+        /// The amd
+        /// </summary>
         AMD = 051,
+
+        /// <summary>
+        /// The ang
+        /// </summary>
         ANG = 532,
+
+        /// <summary>
+        /// The aoa
+        /// </summary>
         AOA = 973,
+
+        /// <summary>
+        /// The ars
+        /// </summary>
         ARS = 032,
+
+        /// <summary>
+        /// The aud
+        /// </summary>
         AUD = 036,
+
+        /// <summary>
+        /// The awg
+        /// </summary>
         AWG = 533,
+
+        /// <summary>
+        /// The azn
+        /// </summary>
         AZN = 944,
+
+        /// <summary>
+        /// The bam
+        /// </summary>
         BAM = 977,
+
+        /// <summary>
+        /// The BBD
+        /// </summary>
         BBD = 052,
+
+        /// <summary>
+        /// The BDT
+        /// </summary>
         BDT = 050,
+
+        /// <summary>
+        /// The BGN
+        /// </summary>
         BGN = 975,
+
+        /// <summary>
+        /// The BHD
+        /// </summary>
         BHD = 048,
+
+        /// <summary>
+        /// The bif
+        /// </summary>
         BIF = 108,
+
+        /// <summary>
+        /// The BMD
+        /// </summary>
         BMD = 060,
+
+        /// <summary>
+        /// The BND
+        /// </summary>
         BND = 096,
+
+        /// <summary>
+        /// The bob
+        /// </summary>
         BOB = 068,
+
+        /// <summary>
+        /// The bov
+        /// </summary>
         BOV = 984,
+
+        /// <summary>
+        /// The BRL
+        /// </summary>
         BRL = 986,
+
+        /// <summary>
+        /// The BSD
+        /// </summary>
         BSD = 044,
+
+        /// <summary>
+        /// The BTN
+        /// </summary>
         BTN = 064,
+
+        /// <summary>
+        /// The BWP
+        /// </summary>
         BWP = 072,
+
+        /// <summary>
+        /// The byn
+        /// </summary>
         BYN = 933,
+
+        /// <summary>
+        /// The BZD
+        /// </summary>
         BZD = 084,
+
+        /// <summary>
+        /// The cad
+        /// </summary>
         CAD = 124,
+
+        /// <summary>
+        /// The CDF
+        /// </summary>
         CDF = 976,
+
+        /// <summary>
+        /// The che
+        /// </summary>
         CHE = 947,
+
+        /// <summary>
+        /// The CHF
+        /// </summary>
         CHF = 756,
+
+        /// <summary>
+        /// The CHW
+        /// </summary>
         CHW = 948,
+
+        /// <summary>
+        /// The CLF
+        /// </summary>
         CLF = 990,
+
+        /// <summary>
+        /// The CLP
+        /// </summary>
         CLP = 152,
+
+        /// <summary>
+        /// The cny
+        /// </summary>
         CNY = 156,
+
+        /// <summary>
+        /// The cop
+        /// </summary>
         COP = 170,
+
+        /// <summary>
+        /// The cou
+        /// </summary>
         COU = 970,
+
+        /// <summary>
+        /// The CRC
+        /// </summary>
         CRC = 188,
+
+        /// <summary>
+        /// The cuc
+        /// </summary>
         CUC = 931,
+
+        /// <summary>
+        /// The cup
+        /// </summary>
         CUP = 192,
+
+        /// <summary>
+        /// The cve
+        /// </summary>
         CVE = 132,
+
+        /// <summary>
+        /// The DJF
+        /// </summary>
         DJF = 262,
+
+        /// <summary>
+        /// The DKK
+        /// </summary>
         DKK = 208,
+
+        /// <summary>
+        /// The dop
+        /// </summary>
         DOP = 214,
+
+        /// <summary>
+        /// The DZD
+        /// </summary>
         DZD = 012,
+
+        /// <summary>
+        /// The egp
+        /// </summary>
         EGP = 818,
+
+        /// <summary>
+        /// The ern
+        /// </summary>
         ERN = 232,
+
+        /// <summary>
+        /// The etb
+        /// </summary>
         ETB = 230,
+
+        /// <summary>
+        /// The FJD
+        /// </summary>
         FJD = 242,
+
+        /// <summary>
+        /// The FKP
+        /// </summary>
         FKP = 238,
+
+        /// <summary>
+        /// The gel
+        /// </summary>
         GEL = 981,
+
+        /// <summary>
+        /// The GHS
+        /// </summary>
         GHS = 936,
+
+        /// <summary>
+        /// The gip
+        /// </summary>
         GIP = 292,
+
+        /// <summary>
+        /// The GMD
+        /// </summary>
         GMD = 270,
+
+        /// <summary>
+        /// The GNF
+        /// </summary>
         GNF = 324,
+
+        /// <summary>
+        /// The GTQ
+        /// </summary>
         GTQ = 320,
+
+        /// <summary>
+        /// The gyd
+        /// </summary>
         GYD = 328,
+
+        /// <summary>
+        /// The HKD
+        /// </summary>
         HKD = 344,
+
+        /// <summary>
+        /// The HNL
+        /// </summary>
         HNL = 340,
+
+        /// <summary>
+        /// The HRK
+        /// </summary>
         HRK = 191,
+
+        /// <summary>
+        /// The HTG
+        /// </summary>
         HTG = 332,
+
+        /// <summary>
+        /// The idr
+        /// </summary>
         IDR = 360,
+
+        /// <summary>
+        /// The ils
+        /// </summary>
         ILS = 376,
+
+        /// <summary>
+        /// The inr
+        /// </summary>
         INR = 356,
+
+        /// <summary>
+        /// The iqd
+        /// </summary>
         IQD = 368,
+
+        /// <summary>
+        /// The irr
+        /// </summary>
         IRR = 364,
+
+        /// <summary>
+        /// The isk
+        /// </summary>
         ISK = 352,
+
+        /// <summary>
+        /// The JMD
+        /// </summary>
         JMD = 388,
+
+        /// <summary>
+        /// The jod
+        /// </summary>
         JOD = 400,
+
+        /// <summary>
+        /// The jpy
+        /// </summary>
         JPY = 392,
+
+        /// <summary>
+        /// The kes
+        /// </summary>
         KES = 404,
+
+        /// <summary>
+        /// The KGS
+        /// </summary>
         KGS = 417,
+
+        /// <summary>
+        /// The KHR
+        /// </summary>
         KHR = 116,
+
+        /// <summary>
+        /// The KMF
+        /// </summary>
         KMF = 174,
+
+        /// <summary>
+        /// The KPW
+        /// </summary>
         KPW = 408,
+
+        /// <summary>
+        /// The KRW
+        /// </summary>
         KRW = 410,
+
+        /// <summary>
+        /// The KWD
+        /// </summary>
         KWD = 414,
+
+        /// <summary>
+        /// The kyd
+        /// </summary>
         KYD = 136,
+
+        /// <summary>
+        /// The KZT
+        /// </summary>
         KZT = 398,
+
+        /// <summary>
+        /// The lak
+        /// </summary>
         LAK = 418,
+
+        /// <summary>
+        /// The LBP
+        /// </summary>
         LBP = 422,
+
+        /// <summary>
+        /// The LKR
+        /// </summary>
         LKR = 144,
+
+        /// <summary>
+        /// The LRD
+        /// </summary>
         LRD = 430,
+
+        /// <summary>
+        /// The LSL
+        /// </summary>
         LSL = 426,
+
+        /// <summary>
+        /// The lyd
+        /// </summary>
         LYD = 434,
+
+        /// <summary>
+        /// The mad
+        /// </summary>
         MAD = 504,
+
+        /// <summary>
+        /// The MDL
+        /// </summary>
         MDL = 498,
+
+        /// <summary>
+        /// The mga
+        /// </summary>
         MGA = 969,
+
+        /// <summary>
+        /// The MKD
+        /// </summary>
         MKD = 807,
+
+        /// <summary>
+        /// The MMK
+        /// </summary>
         MMK = 104,
+
+        /// <summary>
+        /// The MNT
+        /// </summary>
         MNT = 496,
+
+        /// <summary>
+        /// The mop
+        /// </summary>
         MOP = 446,
+
+        /// <summary>
+        /// The MRU
+        /// </summary>
         MRU = 929,
+
+        /// <summary>
+        /// The mur
+        /// </summary>
         MUR = 480,
+
+        /// <summary>
+        /// The MVR
+        /// </summary>
         MVR = 462,
+
+        /// <summary>
+        /// The MWK
+        /// </summary>
         MWK = 454,
+
+        /// <summary>
+        /// The MXN
+        /// </summary>
         MXN = 484,
+
+        /// <summary>
+        /// The MXV
+        /// </summary>
         MXV = 979,
+
+        /// <summary>
+        /// The myr
+        /// </summary>
         MYR = 458,
+
+        /// <summary>
+        /// The MZN
+        /// </summary>
         MZN = 943,
+
+        /// <summary>
+        /// The nad
+        /// </summary>
         NAD = 516,
+
+        /// <summary>
+        /// The NGN
+        /// </summary>
         NGN = 566,
+
+        /// <summary>
+        /// The nio
+        /// </summary>
         NIO = 558,
+
+        /// <summary>
+        /// The nok
+        /// </summary>
         NOK = 578,
+
+        /// <summary>
+        /// The NPR
+        /// </summary>
         NPR = 524,
+
+        /// <summary>
+        /// The NZD
+        /// </summary>
         NZD = 554,
+
+        /// <summary>
+        /// The omr
+        /// </summary>
         OMR = 512,
+
+        /// <summary>
+        /// The pab
+        /// </summary>
         PAB = 590,
+
+        /// <summary>
+        /// The pen
+        /// </summary>
         PEN = 604,
+
+        /// <summary>
+        /// The PGK
+        /// </summary>
         PGK = 598,
+
+        /// <summary>
+        /// The PHP
+        /// </summary>
         PHP = 608,
+
+        /// <summary>
+        /// The PKR
+        /// </summary>
         PKR = 586,
+
+        /// <summary>
+        /// The pyg
+        /// </summary>
         PYG = 600,
+
+        /// <summary>
+        /// The qar
+        /// </summary>
         QAR = 634,
+
+        /// <summary>
+        /// The ron
+        /// </summary>
         RON = 946,
+
+        /// <summary>
+        /// The RSD
+        /// </summary>
         RSD = 941,
+
+        /// <summary>
+        /// The RWF
+        /// </summary>
         RWF = 646,
+
+        /// <summary>
+        /// The sar
+        /// </summary>
         SAR = 682,
+
+        /// <summary>
+        /// The SBD
+        /// </summary>
         SBD = 090,
+
+        /// <summary>
+        /// The SCR
+        /// </summary>
         SCR = 690,
+
+        /// <summary>
+        /// The SDG
+        /// </summary>
         SDG = 938,
+
+        /// <summary>
+        /// The sek
+        /// </summary>
         SEK = 752,
+
+        /// <summary>
+        /// The SGD
+        /// </summary>
         SGD = 702,
+
+        /// <summary>
+        /// The SHP
+        /// </summary>
         SHP = 654,
+
+        /// <summary>
+        /// The SLL
+        /// </summary>
         SLL = 694,
+
+        /// <summary>
+        /// The sos
+        /// </summary>
         SOS = 706,
+
+        /// <summary>
+        /// The SRD
+        /// </summary>
         SRD = 968,
+
+        /// <summary>
+        /// The SSP
+        /// </summary>
         SSP = 728,
+
+        /// <summary>
+        /// The STN
+        /// </summary>
         STN = 930,
+
+        /// <summary>
+        /// The SVC
+        /// </summary>
         SVC = 222,
+
+        /// <summary>
+        /// The syp
+        /// </summary>
         SYP = 760,
+
+        /// <summary>
+        /// The SZL
+        /// </summary>
         SZL = 748,
+
+        /// <summary>
+        /// The THB
+        /// </summary>
         THB = 764,
+
+        /// <summary>
+        /// The TJS
+        /// </summary>
         TJS = 972,
+
+        /// <summary>
+        /// The TMT
+        /// </summary>
         TMT = 934,
+
+        /// <summary>
+        /// The TND
+        /// </summary>
         TND = 788,
+
+        /// <summary>
+        /// The top
+        /// </summary>
         TOP = 776,
+
+        /// <summary>
+        /// The try
+        /// </summary>
         TRY = 949,
+
+        /// <summary>
+        /// The TTD
+        /// </summary>
         TTD = 780,
+
+        /// <summary>
+        /// The TWD
+        /// </summary>
         TWD = 901,
+
+        /// <summary>
+        /// The TZS
+        /// </summary>
         TZS = 834,
+
+        /// <summary>
+        /// The uah
+        /// </summary>
         UAH = 980,
+
+        /// <summary>
+        /// The ugx
+        /// </summary>
         UGX = 800,
+
+        /// <summary>
+        /// The usn
+        /// </summary>
         USN = 997,
+
+        /// <summary>
+        /// The uyi
+        /// </summary>
         UYI = 940,
+
+        /// <summary>
+        /// The uyu
+        /// </summary>
         UYU = 858,
+
+        /// <summary>
+        /// The uyw
+        /// </summary>
         UYW = 927,
+
+        /// <summary>
+        /// The uzs
+        /// </summary>
         UZS = 860,
+
+        /// <summary>
+        /// The ves
+        /// </summary>
         VES = 928,
+
+        /// <summary>
+        /// The VND
+        /// </summary>
         VND = 704,
+
+        /// <summary>
+        /// The vuv
+        /// </summary>
         VUV = 548,
+
+        /// <summary>
+        /// The WST
+        /// </summary>
         WST = 882,
+
+        /// <summary>
+        /// The xaf
+        /// </summary>
         XAF = 950,
+
+        /// <summary>
+        /// The xag
+        /// </summary>
         XAG = 961,
+
+        /// <summary>
+        /// The xau
+        /// </summary>
         XAU = 959,
+
+        /// <summary>
+        /// The xba
+        /// </summary>
         XBA = 955,
+
+        /// <summary>
+        /// The XBB
+        /// </summary>
         XBB = 956,
+
+        /// <summary>
+        /// The XBC
+        /// </summary>
         XBC = 957,
+
+        /// <summary>
+        /// The XBD
+        /// </summary>
         XBD = 958,
+
+        /// <summary>
+        /// The XCD
+        /// </summary>
         XCD = 951,
+
+        /// <summary>
+        /// The XDR
+        /// </summary>
         XDR = 960,
+
+        /// <summary>
+        /// The xof
+        /// </summary>
         XOF = 952,
+
+        /// <summary>
+        /// The XPD
+        /// </summary>
         XPD = 964,
+
+        /// <summary>
+        /// The XPF
+        /// </summary>
         XPF = 953,
+
+        /// <summary>
+        /// The XPT
+        /// </summary>
         XPT = 962,
+
+        /// <summary>
+        /// The xsu
+        /// </summary>
         XSU = 994,
+
+        /// <summary>
+        /// The XTS
+        /// </summary>
         XTS = 963,
+
+        /// <summary>
+        /// The xua
+        /// </summary>
         XUA = 965,
+
+        /// <summary>
+        /// The XXX
+        /// </summary>
         XXX = 999,
+
+        /// <summary>
+        /// The yer
+        /// </summary>
         YER = 886,
+
+        /// <summary>
+        /// The zar
+        /// </summary>
         ZAR = 710,
+
+        /// <summary>
+        /// The ZMW
+        /// </summary>
         ZMW = 967,
+
+        /// <summary>
+        /// The ZWL
+        /// </summary>
         ZWL = 932
     }
 }
